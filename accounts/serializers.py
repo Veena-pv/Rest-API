d934c894a59478b2b673faeecaf1e89d420224fe
Veena P.V <veenapv99@gmail.com>
2023-01-16 16:36:31 +0530

    Initial comment

---
+++ accounts/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+# User Serializer
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username')
+
+# Register Serializer
+class RegisterSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(style={'input_type': 'password'}, write_only=True)
+    password = serializers.CharField(style={'input_type': 'password'}, write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password', 'password2')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def save(self):
+        reg = User(
+            username=self.validated_data['username']
+        )
+        password = self.validated_data['password']
+        password2 = self.validated_data['password2']
+
+        if password != password2:
+            raise serializers.ValidationError({'password': 'Password does not match'})
+        reg.set_password(password)
+        reg.save()
+        return reg
